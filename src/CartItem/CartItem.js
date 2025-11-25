ed94ae990a4405831127d7dc6cc1bc52a0947245
Sky-tales <deyan.tucev2007@gmail.com>
2025-11-25 12:45:46 +0100

    feat: add constructor

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -12,7 +12,10 @@ module.exports = class CartItem {
 
     //region public methods
     constructor(articleId, name, quantity, price) {
-        //TODO Implement this method
+        this._articleId = articleId;
+        this._name = name;
+        this._quantity = quantity;
+        this._price = price;
     }
 
     get articleId() {
